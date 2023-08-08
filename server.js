83a4472079b25c841f4de432910e071d5cb4a2af
Hu <huang.h0709@gmail.com>
2023-08-08 13:43:12 -0400

    fixing url parameters

--- server.js
+++ server.js
@@ -9,13 +9,19 @@ const groceries = ['eggs','noodles','veggies','ice-cream','sauce','rice','drinks
 app.get('/',(req,res)=>{
     res.send("<h1>Here</h1>")
 })
-app.get('/:groceriesIndex',(req,res)=>{
-    if(groceries[req.params.index]){
-        res.send(groceries[req.params.groceriesIndex])
-    }else{
-        res.send('cannot find: ' + req.params.groceriesIndex)
-    }
+app.get('/home',(req,res)=>{
+    res.render("Home")
 })
+app.get('/:groceriesIndex',(req,res)=>{
+    res.send(groceries[req.params.groceriesIndex])
+})
+// app.get('/:groceriesIndex',(req,res)=>{
+//     if(groceries[req.params.index]){
+//         res.send(groceries[req.params.groceriesIndex])
+//     }else{
+//         res.send(`cannot find: ${req.params.groceriesIndex}`)
+//     }
+// })
 let port = 3000
 app.listen(port,(req,res)=>{
     console.log(`Server is now listening ton Port ${port}`)
