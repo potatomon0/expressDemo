a4ff508ece7f31ada5edeb1fec45610fb38a8d64
Hu <huang.h0709@gmail.com>
2023-08-08 13:13:10 -0400

    initial upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+app.set("view engine","jsx")
+app.engine("jsx",require("express-react-views").createEngine())
+
+const groceries = ['eggs','noodles','veggies','ice-cream','sauce','rice','drinks','fish','beef','wings'];
+
+app.get('/',(req,res)=>{
+    res.send("<h1>Here</h1>")
+})
+app.get('/:groceriesIndex',(req,res)=>{
+    if(groceries[req.params.index]){
+        res.send(groceries[req.params.groceriesIndex])
+    }else{
+        res.send('cannot find: ' + req.params.groceriesIndex)
+    }
+})
+let port = 3000
+app.listen(port,(req,res)=>{
+    console.log(`Server is now listening ton Port ${port}`)
+})
